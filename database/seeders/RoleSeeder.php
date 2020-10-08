a86728e9c59642ece9c42773bf91443957322810
ssmavask <ssmavask@gmail.com>
2020-10-08 18:26:45 +0530

    added roles to role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -13,6 +13,42 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Role::truncate();
+        
+        Role::create(['name' => 'admin',
+            'description' => 'description',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Role::create(['name' => 'developer',
+            'description' => 'description',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Role::create(['name' => 'employee',
+            'description' => 'description',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Role::create(['name' => 'deliverymen',
+            'description' => 'description',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Role::create(['name' => 'servicemen',
+            'description' => 'description',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Role::create(['name' => 'vendor',
+            'description' => 'description',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Role::create(['name' => 'customer',
+            'description' => 'description',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
     }
 }
