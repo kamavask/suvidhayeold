f4224b31f75f2db519c1b0e3bf5f8d1d2e30346b
kamavask <kamavask@gmail.com>
2020-10-08 14:13:37 +0530

    updating migrations

---
+++ database/migrations/2020_10_06_183435_create_taxes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taxes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('tax_rate')->nullable();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taxes');
+    }
+}
