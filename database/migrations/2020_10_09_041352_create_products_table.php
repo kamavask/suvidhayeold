74fdfda81e921395645ee9b998200213672213ca
ssmavask <ssmavask@gmail.com>
2020-10-09 13:06:43 +0530

    v0.2

--- database/migrations/2020_10_09_041352_create_products_table.php
+++ database/migrations/2020_10_09_041352_create_products_table.php
@@ -15,7 +15,35 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('prod_code')->nullable();
+            $table->string('prod_name')->nullable();
+            $table->unsignedBigInteger('cat_id')->nullable();
+            $table->string('brand')->nullable();
+            $table->string('highlight')->nullable();
+            $table->string('short_desc')->nullable();
+            $table->string('specification')->nullable();
+            $table->unsignedBigInteger('image_id')->nullable();
+            $table->string('dimensions')->nullable();
+            $table->string('weight')->nullable();
+            $table->string('size')->nullable();
+            $table->boolean('virtual')->nullable();
+            $table->boolean('downloadable')->nullable();
+            $table->string('r_price')->nullable();
+            $table->string('s_price')->nullable();
+            $table->unsignedBigInteger('variant_id')->nullable();
+            $table->string('shy')->nullable();
+            $table->string('stock')->nullable();
+            $table->string('stock_alret')->nullable();
+            $table->string('backorder')->nullable();
+            $table->string('tax_id')->nullable();
+            $table->unsignedBigInteger('shipping_id')->nullable();
+            $table->string('bulk_no')->nullable();
+            $table->string('tags')->nullable();
             $table->timestamps();
+            $table->foreign('cat_id')->references('id')->on('categories');
+            $table->foreign('image_id')->references('id')->on('images');
+            $table->foreign('tax_id')->references('id')->on('taxes');
+            $table->foreign('shipping_id')->references('id')->on('shippings');
         });
     }
 
