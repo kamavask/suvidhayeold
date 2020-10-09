160a7e0e9655f14321f8811fa9640ad6a6b00537
ssmavask <ssmavask@gmail.com>
2020-10-09 19:50:37 +0530

    updated product create method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -24,7 +24,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        return view('admin.pages.add_category');
+        return view('admin.pages.add_product');
         
     }
 
