4b2c35bcb21493d7403afdd803b7f68005c39a1d
ssmavask <ssmavask@gmail.com>
2020-10-09 14:56:13 +0530

    edited adminpagecontroller

--- app/Http/Controllers/AdminPageController.php
+++ app/Http/Controllers/AdminPageController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class AdminPageController extends Controller
 {
-    //
+    /*  taxes Start */
+
+        public function create()
+        {
+            //
+        }
+    
+    /*  taxes end */
+
+    /*  shipping Start */
+
+    /*  shipping End */
+
+    /*  provider Start */
+
+    /*  provider End */
+
+    /*  descriptin Start */
+
+    /*  descriptin Start */
+
+
 }
