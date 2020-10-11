21f4a72e2635ce0cd6d48c2315bae53516247a11
ssmavask <ssmavask@gmail.com>
2020-10-12 00:10:38 +0530

    testing without asset after permission change

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -261,56 +261,56 @@
     </script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/mode/xml/xml.min.js">
     </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/froala_editor.min.js') }}">
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/froala_editor.min.js'">
     </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/align.min.js') }}">
-    </script>
-    <script type="text/javascript"
-        src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/code_beautifier.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/code_view.min.js') }}">
-    </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/colors.min.js') }}">
-    </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/emoticons.min.js') }}">
-    </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/draggable.min.js') }}">
-    </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/font_size.min.js') }}">
-    </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/font_family.min.js') }}">
-    </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/image.min.js') }}">
-    </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/file.min.js') }}">
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/align.min.js">
     </script>
     <script type="text/javascript"
-        src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/image_manager.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/line_breaker.min.js') }}">
+        src="admin/vendor/froala_editor_3.1.1/js/plugins/code_beautifier.min.js"></script>
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/code_view.min.js">
     </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/link.min.js') }}">
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/colors.min.js">
     </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/lists.min.js') }}">
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/emoticons.min.js">
+    </script>
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/draggable.min.js">
+    </script>
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/font_size.min.js">
+    </script>
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/font_family.min.js">
+    </script>
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/image.min.js">
+    </script>
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/file.min.js">
     </script>
     <script type="text/javascript"
-        src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/paragraph_format.min.js') }}"></script>
+        src="admin/vendor/froala_editor_3.1.1/js/plugins/image_manager.min.js"></script>
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/line_breaker.min.js">
+    </script>
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/link.min.js">
+    </script>
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/lists.min.js">
+    </script>
     <script type="text/javascript"
-        src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/paragraph_style.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/video.min.js') }}">
+        src="admin/vendor/froala_editor_3.1.1/js/plugins/paragraph_format.min.js"></script>
+    <script type="text/javascript"
+        src="admin/vendor/froala_editor_3.1.1/js/plugins/paragraph_style.min.js"></script>
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/video.min.js">
     </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/table.min.js') }}">
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/table.min.js">
     </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/url.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/entities.min.js') }}">
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/url.min.js"></script>
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/entities.min.js">
     </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/char_counter.min.js') }}">
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/char_counter.min.js">
     </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/inline_style.min.js') }}">
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/inline_style.min.js">
     </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/save.min.js') }}">
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/save.min.js">
     </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/fullscreen.min.js') }}">
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/fullscreen.min.js">
     </script>
-    <script type="text/javascript" src="{{ asset('admin/vendor/froala_editor_3.1.1/js/plugins/quote.min.js') }}">
+    <script type="text/javascript" src="admin/vendor/froala_editor_3.1.1/js/plugins/quote.min.js">
     </script>
     <script>
         (function () {
