5153cdd00aab05bf627b5cbd0d5a7d09fdc7b52e
kamavask <kamavask@gmail.com>
2020-10-11 14:49:20 +0530

    change in seeder

--- database/seeders/RoleTableSeeder.php
+++ database/seeders/RoleTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 
 class RoleTableSeeder extends Seeder
 {
@@ -13,6 +14,13 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Role::truncate();
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'developer']);
+        Role::create(['name' => 'employee']);
+        Role::create(['name' => 'deliverymen']);
+        Role::create(['name' => 'servicemen']);
+        Role::create(['name' => 'vendor']);
+        Role::create(['name' => 'customer']);
     }
 }
