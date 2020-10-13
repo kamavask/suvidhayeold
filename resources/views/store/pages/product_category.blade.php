72849d43085b2f8c2d14b333472bab9ddb5387ec
ssmavask <ssmavask@gmail.com>
2020-10-14 04:31:16 +0530

    test for products naming 2

--- resources/views/store/pages/product_category.blade.php
+++ resources/views/store/pages/product_category.blade.php
@@ -73,7 +73,7 @@
 										{{-- {{$item->prod_name}} --}}
 										@php
 												$prod_name_raw = $item->prod_name;
-												$prodname = explode(',', $prodname);
+												$prodname = explode(',', $prod_name_raw);
 												echo $prodname;
 										@endphp
 									</h4>
