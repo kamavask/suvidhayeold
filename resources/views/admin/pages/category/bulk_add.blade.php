7fa64433e95db189b6276d9942dd6f6540bc7c44
ssmavask <ssmavask@gmail.com>
2020-10-15 12:52:23 +0530

    added routes to bulk  upload

--- resources/views/admin/pages/category/bulk_add.blade.php
+++ resources/views/admin/pages/category/bulk_add.blade.php
@@ -10,17 +10,18 @@
             <div class="col-lg-12 col-md-12">
               <div class="card card-static-2 mb-30">
                 <div class="card-title-1">
-                  <h4>ulk Add Categorys</h4>
+                  <h4>Bulk Add Categorys</h4>
                 </div>
                 <div class="card-body-table">
                   <div class="news-content-right pd-20">
                     <form action="">
                       <div class="row" style="margin-bottom:1%">
                         <div class="col-12">
-                          <div class="form-group">
-                            <label for="exampleFormControlFile1" class="form-label"><b>Icons</b></label>
-                            <input type="file" class="form-control-file" id="exampleFormControlFile1" name="icon">
-                          </div>
+                          <div class="content">
+                            <div class="box">
+                              <input type="file" name="category_bulk" id="upload_bulf_btn" class="save-btn hover-btn" {{-- data-multiple-caption="{count} files selected" multiple  --}}/>
+                              <label for="upload_bulf_btn"><span>Choose a file</span></label>
+                    				</div>
                         </div>
                       </div>
                       <button class="save-btn hover-btn " type="submit ">Submit</button>
