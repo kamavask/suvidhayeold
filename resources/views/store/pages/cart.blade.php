4aa44f6626d9b2261ea93248a654a95a1cc59670
kamavask <kamavask@gmail.com>
2020-10-17 01:27:19 +0530

    cart page

--- resources/views/store/pages/cart.blade.php
+++ resources/views/store/pages/cart.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 
-<!-- Cart Sidebar Offset Start-->
 <div class="bs-canvas bs-canvas-left position-fixed bg-cart h-100">
   <div class="bs-canvas-header side-cart-header p-3 ">
     <div class="d-inline-block  main-cart-title">My Cart <span>(2 Items)</span></div>
