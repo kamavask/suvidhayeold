f04b55bc267eca4adbcc47e441072f905145ab97
kamavask <kamavask@gmail.com>
2020-10-17 03:06:01 +0530

    roles

--- database/seeders/RoleTableSeeder.php
+++ database/seeders/RoleTableSeeder.php
@@ -14,7 +14,7 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {
-        Role::truncate();
+        //Role::truncate();
         Role::create(['name' => 'admin']);
         Role::create(['name' => 'developer']);
         Role::create(['name' => 'employee']);
