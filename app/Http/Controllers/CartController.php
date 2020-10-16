31e00a5232c98b65dafeb8696db08febde73ccb3
kamavask <kamavask@gmail.com>
2020-10-17 01:24:57 +0530

    cart page

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -14,7 +14,7 @@ class CartController extends Controller
      */
     public function index()
     {
-        return view('store.layout.cart_sidebar');
+        return view('store.pages.cart');
     }
 
     /**
