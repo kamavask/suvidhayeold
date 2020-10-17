658e19d6f45882067d7078301dcbcfb6fb7ceffa
ssmavask <ssmavask@gmail.com>
2020-10-17 13:16:36 +0530

    adding logo png

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,17 +33,19 @@
                 </label>
             </div>
 
+                <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __('register now') }}
+                </a>
+                </div>
+                
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-                <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
-                    {{ __('register now') }}
-                </a>
-                </div>
+                
                 <x-jet-button class="ml-4">
                     {{ __('Login') }}
                 </x-jet-button>
