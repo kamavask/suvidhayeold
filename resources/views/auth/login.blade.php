e858b13479ccd3c1e52ba404d384c1b3c6e0d52a
ssmavask <ssmavask@gmail.com>
2020-10-17 13:01:12 +0530

    adding logo png

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,8 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            {{-- <x-jet-authentication-card-logo /> --}}
+           <img src="{{ asset('store/images/logo/white.png') }}" alt="logo" style="width: 200px;">
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
