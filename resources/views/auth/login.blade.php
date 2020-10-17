3946b56b7122754c6bd5e7b927b77529b0c348a8
ssmavask <ssmavask@gmail.com>
2020-10-17 13:13:54 +0530

    adding logo png

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,11 +39,11 @@
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
+                <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
                     {{ __('register now') }}
                 </a>
-
+                </div>
                 <x-jet-button class="ml-4">
                     {{ __('Login') }}
                 </x-jet-button>
