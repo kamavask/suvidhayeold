1df27cc0d725c0f78422a0013b73e63580528431
ssmavask <ssmavask@gmail.com>
2020-10-17 12:50:04 +0530

    updating login pagr css old page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
     <x-jet-authentication-card>
         <x-slot name="logo">
             {{-- <x-jet-authentication-card-logo /> --}}
-           <img src="{{ asset('store/images/logo/white.jpg') }}" alt="">
+           <img src="{{ asset('store/images/logo/white.jpg') }}" alt="logo" style="width: 200px;">
             <h2>Suvidhaye</h2>
         </x-slot>
 
