448b46c901b73c3cb054b3af1162f73e79911ed7
ssmavask <ssmavask@gmail.com>
2020-10-18 14:02:22 +0530

    changing logo

--- resources/views/store/layout/navbar.blade.php
+++ resources/views/store/layout/navbar.blade.php
@@ -76,7 +76,24 @@
 					<li class="ui dropdown">
 						<a href="#" class="opts_account">
 							@auth
-							<img src="src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}">
+							{{-- <img src="src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}"> --}}
+
+							@if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                            <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                                <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                            </button>
+                        @else
+                            <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                <div>{{ Auth::user()->name }}</div>
+
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        @endif
+
 							{{ Auth::user()->name}}
 							<i class="uil uil-angle-down"></i>
 							@else
