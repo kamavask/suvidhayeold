cd39461af671100167be9cbb518918d4a1524d8d
ssmavask <ssmavask@gmail.com>
2020-10-21 17:18:58 +0530

    working on new search test file

--- resources/views/search-test.blade.php
+++ resources/views/search-test.blade.php
@@ -23,7 +23,18 @@
   </div>
 
 
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+<script>
+    var path = "{{route('main_public_search')}}";
+
+    $('input.typeahead').typeahead({
+      source: function(terms,process){
+        return $.get(path,{terms:terms},function(data){
+          return process(data);
+        });
+      }
+    });
+</script>
 </body>
 </html>
