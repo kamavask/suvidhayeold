777a43d8ab378fb321bc9221513bb33e14bc2181
kamavask <kamavask@gmail.com>
2020-10-22 14:10:58 +0530

    creating mails

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
+use App\Mail\WelcomeMail;
+use Illuminate\Support\Facades\Mail;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -39,6 +41,9 @@ class CreateNewUser implements CreatesNewUsers
             } */
                 function (User $user) {
                     $this->role_user($user);
+                },
+                function (User $user) {
+                    Mail::to($user->email)->send(new WelcomeMail());
                 }
             );
         });
