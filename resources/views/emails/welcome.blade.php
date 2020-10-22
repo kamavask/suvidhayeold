351eaa6a8a9992c53afac7cb9c3efaf12b86165e
kamavask <kamavask@gmail.com>
2020-10-22 18:04:48 +0530

    creating mails

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,10 +1,20 @@
 @component('mail::message')
 # Welcome to Suvidhaye
 
+We are so happy to have you on board. <strong>SUVIDHAYE</strong>, a Jabalpur based Startup, is here with an exciting
+variety of <strong>PRODUCTS
+  AND SERVICES</strong> at your doorstep within <strong>24 hrs.</strong> Ordering of Products and Services <strong>CALL
+  AND ON WHATSAPP</strong> makes it easier to use. We are here with the new feature of <strong>UPLOADING HANDWRITTEN
+  LIST</strong> in just 2 steps. Make your life
+easier with
+Suvidhaye.
+
+|| हर सुविधाएं आपके घर तक ||
+
 
 
 @component('mail::button', ['url' => 'http://suvidhaye.in/'])
-Suvidhaye
+Visit Suvidhaye
 @endcomponent
 
 Thanks,<br>
