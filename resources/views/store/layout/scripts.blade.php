d57a37df1bb74b8d1dd83528afdfc2c14e160e5a
ssmavask <ssmavask@gmail.com>
2020-10-29 18:33:11 +0530

    addiung asset to scripts

--- resources/views/store/layout/scripts.blade.php
+++ resources/views/store/layout/scripts.blade.php
@@ -1,13 +1,13 @@
 <!-- Javascripts -->
-    <script src="/js/jquery-3.3.1.min.js"></script>
-    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="/vendor/OwlCarousel/owl.carousel.js"></script>
-    <script src="/vendor/semantic/semantic.min.js"></script>
-    <script src="/js/jquery.countdown.min.js"></script>
-    <script src="/js/custom.js"></script>
-    <script src="/js/product.thumbnail.slider.js"></script>
-    <script src="/js/offset_overlay.js"></script>
-    <script src="/js/night-mode.js"></script>
+    <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('vendor/OwlCarousel/owl.carousel.js')}}"></script>
+    <script src="{{asset('vendor/semantic/semantic.min.js')}}"></script>
+    <script src="{{asset('js/jquery.countdown.min.js')}}"></script>
+    <script src="{{asset('js/custom.js')}}"></script>
+    <script src="{{asset('js/product.thumbnail.slider.js')}}"></script>
+    <script src="{{asset('js/offset_overlay.js')}}"></script>
+    <script src="{{asset('js/night-mode.js')}}"></script>
 
 {{-- <script src="/js/jquery-3.3.1.min.js"></script> --}}
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
@@ -15,14 +15,14 @@
   integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script> --}}
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"
   integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
-<script src="/vendor/OwlCarousel/owl.carousel.js"></script>
+<script src="{{asset('vendor/OwlCarousel/owl.carousel.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js" integrity="sha512-HWlJyU4ut5HkEj0QsK/IxBCY55n5ZpskyjVlAoV9Z7XQwwkqXoYdCIC93/htL3Gu5H3R4an/S0h2NXfbZk3g7w==" crossorigin="anonymous"></script>
-<script src="/vendor/semantic/semantic.min.js"></script>
-<script src="/js/jquery.countdown.min.js"></script>
-<script src="/js/custom.js"></script>
-<script src="/js/offset_overlay.js"></script>
-<script src="/js/night-mode.js"></script>
-<script>
+<script src="{{asset('vendor/semantic/semantic.min.js')}}"></script>
+<script src="{{asset('js/jquery.countdown.min.js')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
+<script src="{{asset('js/offset_overlay.js')}}"></script>
+<script src="{{asset('js/night-mode.js')}}"></script>
+{{-- <script>
     var path = "{{route('main_public_search')}}";
 
     $('input.typeahead').typeahead({
@@ -33,6 +33,6 @@
       }
     });
 
-</script>
+</script> --}}
 
   <!-- Javascripts -->
