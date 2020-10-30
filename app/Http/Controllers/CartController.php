9a4b41535829684a43619d5b2cd00b59edceba8c
kamavask <kamavask@gmail.com>
2020-10-30 14:56:47 +0530

    echoping success message

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -39,7 +39,7 @@ class CartController extends Controller
         // dd($request->s_price);
         $cartitem = Cart::add($request->id, $request->name, $request->s_price, 1);
 
-        return redirect()->route('cart.index')->with('success_message', 'Item added to your cart');
+        return redirect()->route('cart.index')->with('success_message', $cartitem);
     }
 
     /**
