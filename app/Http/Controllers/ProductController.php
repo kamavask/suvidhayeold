92902675979bd22684cbe98c8fc3dc7c129e9b96
scmavask <scmavask@gmail.com>
2020-10-31 17:26:11 +0530

    adding Edit_producty functions

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -39,20 +39,20 @@ class ProductController extends Controller
         $prod->prod_name = $request->prod_name;
         $prod->virtual = $request->virtual;
         $prod->downloadable = $request->downloadable;
-        $prod->image_id = $request->image_id;
+        /* $prod->image_id = $request->image_id; */
         $prod->highlight = $request->highlight;
         $prod->short_description = $request->short_desc;
-        $prod->specification = $request->specification;
+        /* $prod->specification = $request->specification; */
         $prod->brand = $request->brand;
         $prod->weight = $request->weight;
-        $prod->size = $request->size;
-        $prod->dimensions = $request->dimensions;
+        /* $prod->size = $request->size; */
+       /*  $prod->dimensions = $request->dimensions;
         $prod->category_id = $request->cat_id;
-        $prod->variant_id = $request->variant_id;
+        $prod->variant_id = $request->variant_id; */
         $prod->r_price = $request->r_price;
         $prod->s_price = $request->s_price;
-        $prod->tax_id = $request->tax_id;
-        $prod->shipping_id = $request->shipping_id;
+       /*  $prod->tax_id = $request->tax_id;
+        $prod->shipping_id = $request->shipping_id; */
         $prod->sku = $request->sku;
         $prod->stock = $request->stock;
         $prod->stock_alert = $request->stock_alert;
@@ -87,7 +87,8 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        $prod = Product::find($id);
+        return view('admin.pages.category.edit_product', compact('prod', 'id'));
     }
 
     /**
@@ -99,7 +100,57 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $this->validate($request, [
+            'prod_name' => 'required',
+            'virtual' => 'required',
+            'downloadable' => 'required',
+            'image_id' => 'required',
+            'highlights' => 'required',
+            'short_desc' => 'required',
+            'specification' => 'required',
+            'brand' => 'required',
+            'weight' => 'required',
+            'size' => 'required',
+            'dimensions' => 'required',
+            'cat_id' => 'required',
+            'varient_id' => 'required',
+            'r_price' => 'required',
+            's_price' => 'required',
+            'tax_id' => 'required',
+            'shipping_id' => 'required',
+            'sku' => 'required',
+            'stock' => 'required',
+            'stock_alert' => 'required',
+            'backorder' => 'required',
+            'bulk_no' => 'required',
+            'tags' => 'required'
+        ]);
+        $prod = Product::find($id);
+        $prod->prod_name = $request->get('prod_name');
+        $prod->virtual = $request->get('virtual');
+        $prod->downloadable = $request->get('downloadable');
+        $prod->image_id = $request->get('image_id');
+        $prod->highlight = $request->get('highlight');
+        $prod->short_description = $request->get('short_desc');
+        $prod->specification = $request->get('specification');
+        $prod->brand = $request->get('brand');
+        $prod->weight = $request->get('weight');
+        $prod->size = $request->get('size'); 
+        $prod->dimensions = $request->get('dimensions');
+        $prod->category_id = $request->get('cat_id');
+        $prod->variant_id = $request->get('variant_id'); 
+        $prod->r_price = $request->get('r_price');
+        $prod->s_price = $request->get('s_price');
+        $prod->tax_id = $request->get('tax_id');
+        $prod->shipping_id = $request->get('shipping_id'); 
+        $prod->sku = $request->get('sku');
+        $prod->stock = $request->get('stock');
+        $prod->stock_alert = $request->get('stock_alert');
+        $prod->backorder = $request->get('backorder');
+        $prod->bulk_order_no = $request->('bulk_no');
+        $prod->tags = $request->get('tags');
+        $prod->save();
+        return redirect()->route('admin.pages.product.add_product')->with('success', 'Data Updated');
     }
 
     /**
--- resources/views/admin/pages/product/edit_product.blade.php
+++ resources/views/admin/pages/product/edit_product.blade.php
@@ -11,6 +11,9 @@
                         <li class="breadcrumb-item"><a href="products.html">Products</a></li>
                         <li class="breadcrumb-item active">Edit Product</li>
                     </ol>
+                     <form method="post" action="{{action('ProductController@update' , $id)}}">
+                                {{csrf_field()}}
+                                <input type="hidden" name="_method" value="PATCH" />
                     <div class="row">
                         <div class="col-lg-12 col-md-12">
                             <div class="card card-static-2 mb-30">
@@ -24,20 +27,20 @@
                                             <div class="col-sm-8">
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Product Name</b></label>
-                                                    <input type="text" class="form-control" placeholder="Add Product Name" name="prod_name">
+                                                <input type="text" class="form-control" placeholder="Add Product Name" name="prod_name" value="{{prod->prod_name}}">
                                                 </div>
                                             </div>
                                             <div class="col-sm-1">
                                                 <!-- Default checked -->
                                                 <div class="custom-control custom-switch" style="margin-top: 35px;">
-                                                    <input type="checkbox" class="custom-control-input" id="customSwitch1" name="virtual">
+                                                    <input type="checkbox" class="custom-control-input" id="customSwitch1" name="virtual" value="{{prod->virtual}}">
                                                     <label class="custom-control-label" for="customSwitch1">Virtuals</label>
                                                 </div>
                                             </div>
                                             <div class="col-sm-2">
                                                 <!-- Default switch -->
                                                 <div class="custom-control custom-switch" style="margin-top:35px">
-                                                    <input type="checkbox" class="custom-control-input" id="customSwitches" name="downloadable">
+                                                    <input type="checkbox" class="custom-control-input" id="customSwitches" name="downloadable" value="{{prod->downloadable}}">
                                                     <label class="custom-control-label" for="customSwitches">Downloadable</label>
                                                 </div>
                                             </div>
@@ -47,7 +50,7 @@
                                             <div class="col-12">
                                                 <div class="form-group">
                                                     <label for="exampleFormControlFile1" class="form-label"><b>Image ID</b></label>
-                                                    <input type="file" class="form-control-file" id="exampleFormControlFile1" name="image_id">
+                                                    <input type="file" class="form-control-file" id="exampleFormControlFile1" name="image_id" value="{{prod->image_id}}">
                                                 </div>
                                             </div>
                                         </div>
@@ -56,7 +59,7 @@
                                             <div class="col-sm-12">
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Highlights</b></label>
-                                                    <textarea class="form-control " rows="2 " placeholder="Enter Highlights " name="highlight " id="highlights "></textarea>
+                                                    <textarea class="form-control " rows="2 " placeholder="Enter Highlights " name="highlight " id="highlights " value="{{prod->highlight}}"></textarea>
                                                 </div>
                                             </div>
                                         </div>
@@ -65,7 +68,7 @@
                                             <div class="col-sm-12">
                                                 <div class="form-group ">
                                                     <label class="form-label"><b>Short Discription</b></label>
-                                                    <textarea class="form-control " rows="3 " placeholder="Enter Description " name="short_desc " id="s_desc "></textarea>
+                                                    <textarea class="form-control " rows="3 " placeholder="Enter Description " name="short_desc " id="s_desc" value="{{prod->short_desc}}"></textarea>
                                                 </div>
                                             </div>
                                         </div>
@@ -74,7 +77,7 @@
                                             <div class="col-sm-12">
                                                 <div class="form-group ">
                                                     <label class="form-label"><b>Specifications</b></label>
-                                                    <textarea class="form-control " rows="4 " placeholder="Enter Specifications " name="specification " id="speci "></textarea>
+                                                    <textarea class="form-control " rows="4 " placeholder="Enter Specifications " name="specification " id="speci " value="{{prod->specification}}"></textarea>
                                                 </div>
                                             </div>
                                         </div>
@@ -83,7 +86,7 @@
                                             <div class="col-sm-12">
                                                 <div class="form-group ">
                                                     <label class="form-label"><b>Brand</b></label>
-                                                    <input type="text " class="form-control " placeholder="Enter Provider Code " name="brand " id="pro ">
+                                                    <input type="text " class="form-control " placeholder="Enter Provider Code " name="brand " id="pro " value="{{prod->brand}}">
                                                 </div>
                                             </div>
                                         </div>
@@ -93,14 +96,14 @@
                                                 <!-- text input -->
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Weights</b></label>
-                                                    <input type="text" class="form-control" placeholder="Enter Weight (e.g.:30 Kg)" name="weight" id="weight">
+                                                    <input type="text" class="form-control" placeholder="Enter Weight (e.g.:30 Kg)" name="weight" id="weight" value="{{prod->weight}}">
                                                 </div>
                                             </div>
 
                                             <div class="col-sm-6">
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Sizes</b></label>
-                                                    <select class="form-control" name="size">
+                                                    <select class="form-control" name="size" value="{{prod->size}}">
                                                         <option>XS (Extra Small)</option>
                                                         <option>S (Small)</option>
                                                         <option>M (Medium)</option>
@@ -116,7 +119,7 @@
                                                 <!-- text input -->
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Dimensions</b></label>
-                                                    <input type="text" class="form-control" placeholder="Length-Bregth-height (e.g.:12cm-4cm-7cm)" name="dimensions" id="dimension">
+                                                    <input type="text" class="form-control" placeholder="Length-Bregth-height (e.g.:12cm-4cm-7cm)" name="dimensions" id="dimension" value="{{prod->dimensions}}">
                                                 </div>
                                             </div>
 
@@ -124,7 +127,7 @@
                                                 <!-- text input -->
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Category ID</b></label>
-                                                    <input type="text" class="form-control" placeholder="Enter ID" name="cat_id" id="Category_ID">
+                                                    <input type="text" class="form-control" placeholder="Enter ID" name="cat_id" id="Category_ID" value="{{prod->cat_id}}">
                                                 </div>
                                             </div>
                                         </div>
@@ -134,7 +137,7 @@
                                                 <!-- text input -->
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Varient ID</b></label>
-                                                    <input type="text" class="form-control" placeholder="Enter ID" name="variend_id" id="v_id">
+                                                    <input type="text" class="form-control" placeholder="Enter ID" name="variend_id" id="v_id" value="{{prod->varient_id}}">
                                                 </div>
                                             </div>
                                         </div>
@@ -150,7 +153,7 @@
                                                         <div class="input-group-prepend">
                                                             <span class="input-group-text"><i class="fas fa-rupee-sign"></i></span>
                                                         </div>
-                                                        <input type="text" class="form-control" placeholder="Enter Price" name="r_price">
+                                                        <input type="text" class="form-control" placeholder="Enter Price" name="r_price" value="{{prod->r_price}}">
 
                                                     </div>
                                                 </div>
@@ -163,7 +166,7 @@
                                                         <div class="input-group-prepend">
                                                             <span class="input-group-text"><i class="fas fa-rupee-sign"></i></span>
                                                         </div>
-                                                        <input type="text" class="form-control" placeholder="Enter Price" name="s_price">
+                                                        <input type="text" class="form-control" placeholder="Enter Price" name="s_price" value="{{prod->s_price}}">
 
                                                     </div>
                                                 </div>
@@ -175,14 +178,14 @@
                                                 <!-- text input -->
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Tax ID</b></label>
-                                                    <input type="text" class="form-control" placeholder="Enter Text ID" name="tax_id" id="tax_id">
+                                                    <input type="text" class="form-control" placeholder="Enter Text ID" name="tax_id" id="tax_id" value="{{prod->tax_id}}">
                                                 </div>
                                             </div>
                                             <div class="col-sm-6">
                                                 <!-- text input -->
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Shipping ID</b></label>
-                                                    <input type="text" class="form-control" placeholder="Enter Shipping ID" name="shipping_id" id="shipping_id">
+                                                    <input type="text" class="form-control" placeholder="Enter Shipping ID" name="shipping_id" id="shipping_id" value="{{prod->shipping_id}}">
                                                 </div>
                                             </div>
                                         </div>
@@ -194,21 +197,21 @@
                                                 <!-- text input -->
                                                 <div class="form-group">
                                                     <label class="form-label"><b>SKU</b></label>
-                                                    <input type="text" class="form-control" placeholder="Enter SKU" name="sku" id="sku">
+                                                    <input type="text" class="form-control" placeholder="Enter SKU" name="sku" id="sku" value="{{prod->sku}}">
                                                 </div>
                                             </div>
                                             <div class="col-sm-3">
                                                 <!-- text input -->
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Stock</b></label>
-                                                    <input type="number" class="form-control" placeholder="Enter stock" name="stock" id="stock">
+                                                    <input type="number" class="form-control" placeholder="Enter stock" name="stock" id="stock" value="{{prod->stock}}">
                                                 </div>
                                             </div>
                                             <div class="col-sm-3">
                                                 <!-- text input -->
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Stock Alert</b></label>
-                                                    <input type="number" class="form-control" placeholder="Enter Stock Alert" name="stock_alert" id="s_alert">
+                                                    <input type="number" class="form-control" placeholder="Enter Stock Alert" name="stock_alert" id="s_alert" value="{{prod->stock_alert}}">
                                                 </div>
                                             </div>
                                         </div>
@@ -217,7 +220,7 @@
                                             <div class="col-sm-3">
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Back-Order Options</b></label>
-                                                    <select class="form-control" name="backorder">
+                                                    <select class="form-control" name="backorder" value="{{prod->backorder}}">
                                                         <option>option 1</option>
                                                         <option>option 2</option>
                                                         <option>option 3</option>
@@ -230,7 +233,7 @@
                                                 <!-- text input -->
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Max Number of order in bulk</b></label>
-                                                    <input type="number" class="form-control" placeholder="Enter Number of Orders" name="bulk_no" id="bulk">
+                                                    <input type="number" class="form-control" placeholder="Enter Number of Orders" name="bulk_no" id="bulk" value="{{prod->bulk_no}}">
                                                 </div>
                                             </div>
                                         </div>
@@ -243,12 +246,12 @@
                                                 <!-- text input -->
                                                 <div class="form-group">
                                                     <label class="form-label"><b>Tags</b></label>
-                                                    <input type="text" class="form-control" placeholder="Enter Tags" name="tags" id="tag">
+                                                    <input type="text" class="form-control" placeholder="Enter Tags" name="tags" id="tag" value="{{prod->tags}}">
                                                 </div>
                                             </div>
 
                                         </div>
-                                        <button class="save-btn hover-btn " type="submit ">Next Page</button>
+                                        <button class="save-btn hover-btn " type="submit ">Edit</button>
                                     </div>
                                 </div>
                             </div>
