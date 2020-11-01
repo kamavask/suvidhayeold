bb0a63add8c886da9dfde23d80314c90dce4ba80
ssmavask <ssmavask@gmail.com>
2020-11-01 23:49:04 +0530

    product show page work pagination

--- resources/views/admin/pages/product/show_product.blade.php
+++ resources/views/admin/pages/product/show_product.blade.php
@@ -3,10 +3,10 @@
 
 
                     <div class="container-fluid">
-                        <h2 class="mt-30 page-title">Areas</h2>
+                        <h2 class="mt-30 page-title">Products</h2>
                         <ol class="breadcrumb mb-30">
                             <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Areas</li>
+                            <li class="breadcrumb-item active">Products</li>
                         </ol>
                         <div class="row justify-content-between">
 							<div class="col-lg-12">
@@ -79,6 +79,9 @@
 										</div>
 									</div>
 								</div>
+                            </div>
+                            <div class="col-lg-12">
+								{{$users->onEachSide(5)->links()}}
 							</div>
                         </div>
                     </div>
