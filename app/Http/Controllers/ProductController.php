0822445b886de4c712e0813f66649946fb50973a
ssmavask <ssmavask@gmail.com>
2020-11-01 23:43:06 +0530

    product show page work pagination

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ProductController extends Controller
 {
