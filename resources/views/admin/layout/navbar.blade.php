897bea8a9bb2d31729d175ff7e6c017c91b920cd
ssmavask <ssmavask@gmail.com>
2020-11-01 13:28:52 +0530

    admin panel user avatart section

--- resources/views/admin/layout/navbar.blade.php
+++ resources/views/admin/layout/navbar.blade.php
@@ -13,8 +13,7 @@
     <ul class="navbar-nav ml-auto mr-md-0">
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                {{-- <i class="fas fa-user fa-fw"></i> --}}
-                <div>{{ Auth::user()->name }}</div>
+                <i class="fas fa-user fa-fw"></i>
             </a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
                 <a class="dropdown-item admin-dropdown-item" href="edit_profile.html">
