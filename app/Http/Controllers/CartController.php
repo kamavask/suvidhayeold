4a6a60779e248b55f7fcd1f733940a6f50ab3380
kamavask <kamavask@gmail.com>
2020-11-03 18:49:04 +0530

    cart page

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -56,7 +56,7 @@ class CartController extends Controller
         //dd($id);
         // $cartitem = ShoppingCart::where('user_id', $id)->get();
         $cartitem = User::find($id)->Product;
-        dd($cartitem);
+        return $cartitem;
         //return view('cart', compact('cartitem'));
 
         //$flag = flag::find(1)->Product;
