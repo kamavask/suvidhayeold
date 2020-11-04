12013c9f1261bf5bddb3f71df18c6952a2053e90
kamavask <kamavask@gmail.com>
2020-11-04 17:40:01 +0530

    changing function to product_cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -55,7 +55,7 @@ class CartController extends Controller
     {
         //dd($id);
         // $cartitem = ShoppingCart::where('user_id', $id)->get();
-        $cartitem = User::find($id)->Product;
+        $cartitem = User::find($id)->Product_cart;
         //dd($cartitem);
         return view('cart', compact('cartitem'));
 
