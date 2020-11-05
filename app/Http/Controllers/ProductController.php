da79df7dd006bfdd754d0113662f08caf539f330
scmavask <scmavask@gmail.com>
2020-11-05 15:57:55 +0530

    product controller show function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -38,75 +38,29 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         $prod = new Product;
-        if ($prod->prod_name != null) {
-            $prod->prod_name = $request->prod_name;
-        }
-        if ($prod->virtual != null) {
-            $prod->virtual = $request->virtual;
-        }
-        if ($prod->downloadable != null) {
-            $prod->downloadable = $request->downloadable;
-        }
-        if ($prod->image_id != null) {
-            $prod->image_id = $request->image_id;
-        }
-        if ($prod->highlight != null) {
-            $prod->highlight = $request->highlight;
-        }
-        if ($prod->short_desc != null) {
-            $prod->short_description = $request->short_desc;
-        }
-        if ($prod->specification != null) {
-            $prod->specification = $request->specification;
-        }
-        if ($prod->brand != null) {
-            $prod->brand = $request->brand;
-        }
-        if ($prod->weight != null) {
-            $prod->weight = $request->weight;
-        }
-        if ($prod->size != null) {
-            $prod->size = $request->size;
-        }
-        if ($prod->dimensions != null) {
-            $prod->dimensions = $request->dimensions;
-        }
-        if ($prod->cat_id != null) {
-            $prod->category_id = $request->cat_id;
-        }
-        if ($prod->varient_id != null) {
-            $prod->varient_id = $request->varient_id;
-        }
-        if ($prod->r_price != null) {
-            $prod->r_price = $request->r_price;
-        }
-        if ($prod->s_price != null) {
-            $prod->s_price = $request->s_price;
-        }
-        if ($prod->tax_id != null) {
-            $prod->tax_id = $request->tax_id;
-        }
-        if ($prod->shipping_id != null) {
-            $prod->shipping_id = $request->shipping_id;
-        }
-        if ($prod->sku != null) {
-            $prod->sku = $request->sku;
-        }
-        if ($prod->stock != null) {
-            $prod->stock = $request->stock;
-        }
-        if ($prod->stock_alert != null) {
-            $prod->stock_alert = $request->stock_alert;
-        }
-        if ($prod->backorder != null) {
-            $prod->backorder = $request->backorder;
-        }
-        if ($prod->bulk_no != null) {
-            $prod->bulk_order_no = $request->bulk_no;
-        }
-        if ($prod->tags != null) {
-            $prod->tags = $request->tags;
-        }
+        $prod->prod_name = $request->prod_name;
+        $prod->virtual = $request->virtual;
+        $prod->downloadable = $request->downloadable;
+        $prod->image_id = $request->image_id; 
+        $prod->highlight = $request->highlight;
+        $prod->short_description = $request->short_desc; 
+        $prod->specification = $request->specification; 
+        $prod->brand = $request->brand;
+        $prod->weight = $request->weight; 
+        $prod->size = $request->size; 
+        $prod->dimensions = $request->dimensions;
+        $prod->category_id = $request->cat_id;
+        $prod->variant_id = $request->variant_id; 
+        $prod->r_price = $request->r_price;
+        $prod->s_price = $request->s_price; 
+        $prod->tax_id = $request->tax_id;
+        $prod->shipping_id = $request->shipping_id;  
+        $prod->sku = $request->sku;
+        $prod->stock = $request->stock;
+        $prod->stock_alert = $request->stock_alert; 
+        $prod->backorder = $request->backorder;
+        $prod->bulk_order_no = $request->bulk_no;
+        $prod->tags = $request->tags;
         $prod->save();
 
         $product = $prod;  
