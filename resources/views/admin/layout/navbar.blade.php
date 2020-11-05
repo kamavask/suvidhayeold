62d9e7f02546b6925c363bb226d2feed0645bdc3
ssmavask <ssmavask@gmail.com>
2020-11-05 13:04:08 +0530

    added style to admin nav logo

--- resources/views/admin/layout/navbar.blade.php
+++ resources/views/admin/layout/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="sb-topnav navbar navbar-expand navbar-light bg-clr">
     <a class="navbar-brand logo-brand" href="index.html">
-        <img style="width: 200px;" src="/store/images/logo/white.png" alt="">
+        <img style="width: 200px; margin-top: -40px; margin-left: -20px;" src="/store/images/logo/white.png" alt="">
     </a>
     <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#">
         <i class="fas fa-bars"></i>
