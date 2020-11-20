82fba72d80f4989d6ffb7d699d801c623236b5a5
scmavask <scmavask@gmail.com>
2020-11-20 15:33:53 +0530

    redirect based on role

--- resources/views/store/pages/user_homepage.blade.php
+++ resources/views/store/pages/user_homepage.blade.php
@@ -1,4 +1,4 @@
-@extends('store.layout.app')
+@extends('customer.layout.app')
 @section('content')
   <div class="wrapper">
 		<div class="blog-dt-vw banner-blog banner.visible parallax">
@@ -10,12 +10,14 @@
             <br>
 					<div class="row">
 						<div class="col-lg-12 col-md-12">
-							<h1>Blog Title Here</h1>
+							<h1>This Site is now Under Development</h1>
+							<h4>Contact Us Here on Whatsapp for any Order</h4>
 							<div class="extra-info">
-								<span class="entry-date">Tuesday, May 19, 2020</span>
-								<div class="single-post-cat">
-									<a href="#">Food &amp; Lifestyle</a>
-								</div>
+								<span class="entry-date">7024027407</span>
+							</div>
+								<h4>On Call Order also Available</h4>
+							<div class="extra-info">
+								<span class="entry-date">0761-3590646</span>
 							</div>
 						</div>
 					</div>
