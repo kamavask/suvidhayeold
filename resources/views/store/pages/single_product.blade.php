192053cee3a5e777a96b1f1b9a103eda360fbf47
ssmavask <ssmavask@gmail.com>
2020-11-25 16:23:46 +0530

    image test single prod

--- resources/views/store/pages/single_product.blade.php
+++ resources/views/store/pages/single_product.blade.php
@@ -26,7 +26,7 @@
 								<div class="col-lg-4 col-md-4">
                   <div id="sync1" class="owl-carousel owl-theme">
                       <div class="item">
-                          <img src="/store/images/product/big-1.jpg" alt="">
+                          <img src="/storage/images/products/{{$item->prod_name}}.jpg" alt="">
                       </div>
                       <div class="item">
                           <img src="/store/images/product/big-2.jpg" alt="">
@@ -40,7 +40,7 @@
                   </div>
                   <div id="sync2" class="owl-carousel owl-theme">
                       <div class="item">
-                          <img src="/store/images/product/big-1.jpg" alt="">
+                          <img src="/storage/images/products/{{$item->prod_name}}.jpg" alt="">
                       </div>
                       <div class="item">
                           <img src="/store/images/product/big-2.jpg" alt="">
