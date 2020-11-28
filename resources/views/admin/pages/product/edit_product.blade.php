049769904295c09c2fbaeef290d08f891b735a52
ssmavask <ssmavask@gmail.com>
2020-11-28 16:25:31 +0530

    route update prod update form

--- resources/views/admin/pages/product/edit_product.blade.php
+++ resources/views/admin/pages/product/edit_product.blade.php
@@ -7,7 +7,7 @@
             <li class="breadcrumb-item"><a href="products.html">Products</a></li>
             <li class="breadcrumb-item active">Edit Product</li>
         </ol>
-        <form method="POST" action="/admin/pages/product/{{$prod->id}}/edit">
+        <form method="POST" action="/admin/pages/product/{{$prod->id}}">
             @csrf
             {{-- <input type="hidden" name="id" value="{{$prod->id}}"> --}}
             <div class="row">
