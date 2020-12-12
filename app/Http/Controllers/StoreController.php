f67acc0700dbfc589fe1844c3bfc275ba618d05d
ssmavask <ssmavask@gmail.com>
2020-12-12 13:01:41 +0530

    vegies section home page

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -28,7 +28,7 @@ class StoreController extends Controller
         $flag = flag::find(1)->Product;
         $veggiesid = Category::select('id')->where('category_name', 'Vegetables')->first();
         $veggies = Category::find($veggiesid->id)->Product;
-        return view('store.pages.homepage')->with('featured', $flag )->with('vegetables', $veggies );
+        return view('store.pages.homepage' )->with('featured', $flag )->with('vegetables', $veggies );
     }
 
     public function single_product()
