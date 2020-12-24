301b05fd2ef6db705ecfeed78e6def3c7d53a33c
ssmavask <ssmavask@gmail.com>
2020-12-24 22:28:02 +0530

    paginate

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
