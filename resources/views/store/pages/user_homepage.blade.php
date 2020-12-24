d4222cb87f164b86e3eb7915125280400b9573ee
ssmavask <ssmavask@gmail.com>
2020-12-24 13:28:28 +0530

    home page

--- resources/views/store/pages/user_homepage.blade.php
+++ resources/views/store/pages/user_homepage.blade.php
@@ -10,6 +10,7 @@
 						<br> --}}
 						<div class="row">
 						<div class="col-lg-12 col-md-12">
+							<img src="store\images\logo\white.png" alt="" style="border: none;">
 							<h1>This Site is now Under Development</h1>
 							<h4>Contact Us Here on Whatsapp for any Order</h4>
 							<div class="extra-info">
