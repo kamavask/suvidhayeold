08dc85bd0899928d383d6487107fc5af6fc7a114
ssmavask <ssmavask@gmail.com>
2020-12-24 13:26:41 +0530

    home page

--- resources/views/store/pages/user_homepage.blade.php
+++ resources/views/store/pages/user_homepage.blade.php
@@ -4,10 +4,23 @@
 		<div class="blog-dt-vw banner-blog banner.visible parallax" style="background-image: url(images/banners/info_background2.jpg);">
 			<div class="blog-inner" style="background-color: transparent; color: black;">
 				<div class="container">
-            <div class="cart-product-img" style="width: 250px;">
+            {{-- <div class="cart-product-img" style="width: 250px;">
               <img src="store\images\logo\white.png" alt="" style="margin-left: 440px; border: none;">
             </div>
-            <br>
+						<br> --}}
+						<div class="row">
+						<div class="col-lg-12 col-md-12">
+							<h1>This Site is now Under Development</h1>
+							<h4>Contact Us Here on Whatsapp for any Order</h4>
+							<div class="extra-info">
+								<span class="entry-date">7024027407</span>
+							</div>
+								<h4>On Call Order also Available</h4>
+							<div class="extra-info">
+								<span class="entry-date">0761-3590646</span>
+							</div>
+						</div>
+					</div>
 					<div class="row">
 						<div class="col-lg-12 col-md-12">
 							<h1>This Site is now Under Development</h1>
