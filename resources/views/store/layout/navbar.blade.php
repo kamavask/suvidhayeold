8ccc49f7e3dea2770b0b4713f91369f9e405ff06
ssmavask <ssmavask@gmail.com>
2020-12-24 18:17:55 +0530

    name in h4 tag

--- resources/views/store/layout/navbar.blade.php
+++ resources/views/store/layout/navbar.blade.php
@@ -80,8 +80,7 @@
 							@auth
 									@if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
 										<button class="user-avatar-ui">
-											<img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
-												alt="{{ Auth::user()->name }}" />
+											<img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
 										</button>
 										@else
 										<button class="user-avatar-ui">
@@ -89,14 +88,12 @@
 										
 											<div class="ml-1">
 												<svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-													<path fill-rule="evenodd"
-														d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-														clip-rule="evenodd" />
+													<path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
 												</svg>
 											</div>
 										</button>
 									@endif
-									{{ Auth::user()->name}}
+									<h4 class="title_name">{{ Auth::user()->name}}</h4>
 									<i class="uil uil-angle-down"></i>
 								@else
 									Login / signup
