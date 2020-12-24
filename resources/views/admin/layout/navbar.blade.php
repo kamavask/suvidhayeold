1e89bf361532f60cdc7e5b5ba7c46ba56154428b
ssmavask <ssmavask@gmail.com>
2020-12-24 17:14:46 +0530

    htags

--- resources/views/admin/layout/navbar.blade.php
+++ resources/views/admin/layout/navbar.blade.php
@@ -13,14 +13,16 @@
         <li class="breadcrumb-item">
             @if(session()->has('success_message'))
         	    <div class="alert alert-success">
-        	        {{session()->get('success_message')}}
+        	        <h1>{{session()->get('success_message')}}</h1>
         	    </div>
         	@endif
         	@if(count($errors) >0)
         	    <div class="alert alert-danger">
         	        <ul>
         	            @foreach($errors->all() as $error)
-        	            <li>{{ $error }}</li>
+        	            <li>
+                            <h2>{{ $error }}</h2>
+                        </li>
         	            @endforeach
         	        </ul>
         	    </div>
