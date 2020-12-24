a2b499cbc924127b38f9dce12a82278533ad26fd
ssmavask <ssmavask@gmail.com>
2020-12-24 21:17:21 +0530

    admin deshboard uri

--- resources/views/store/layout/navbar.blade.php
+++ resources/views/store/layout/navbar.blade.php
@@ -113,9 +113,9 @@
                 </a>
               </div>
 							@auth
-							@can('access-dashboard')
-								<a href="dashboard_overview" class="item channel_item"><i class="uil uil-apps icon__1"></i>Dashbaord</a>
-							@endcan
+								@can('access-dashboard')
+									<a href="/admin-dashboard" class="item channel_item"><i class="uil uil-apps icon__1"></i>Admin Dashbaord</a>
+								@endcan
 							<a href="dashboard_my_orders" class="item channel_item"><i class="uil uil-box icon__1"></i>My Orders</a>
 							{{-- <a href="dashboard_my_wishlist" class="item channel_item"><i class="uil uil-heart icon__1"></i>My
 								Wishlist</a>
