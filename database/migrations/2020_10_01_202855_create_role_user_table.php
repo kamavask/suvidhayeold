f96100222f4b92bc5d5085b12da4439190383b57
ssmavask <ssmavask@gmail.com>
2021-01-14 21:41:47 +0530

    role user table update

--- database/migrations/2020_10_01_202855_create_role_user_table.php
+++ database/migrations/2020_10_01_202855_create_role_user_table.php
@@ -15,8 +15,8 @@ class CreateRoleUserTable extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('role_id')->unsigned();
-            $table->bigInteger('user_id')->unsigned();
+            $table->unsignedBigInteger('role_id')->unsigned();
+            $table->unsignedBigInteger('user_id')->unsigned();
             $table->foreign('role_id')->references('id')->on('roles');
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
