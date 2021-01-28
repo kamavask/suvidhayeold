01bbae80e784cf722d9ef83f0b2a47f6e09a8132
ssmavask <ssmavask@gmail.com>
2021-01-28 16:52:32 +0530

    head style repair

--- resources/views/store/layout/head.blade.php
+++ resources/views/store/layout/head.blade.php
@@ -1,4 +1,5 @@
 <head>
+
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, shrink-to-fit=9">
@@ -14,8 +15,8 @@
   <link rel="icon" type="image/png" href="{{asset('store/main_images/logo/suvidhaye_logo.png')}}">
 
   <script src="https://kit.fontawesome.com/d25269cf83.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   <link href="{{asset('vendor/OwlCarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
   
   <!-- Stylesheets -->
