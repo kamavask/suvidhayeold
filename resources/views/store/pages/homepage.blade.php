60820109b116503455c2c09ac481e44d4dd729e4
ssmavask <ssmavask@gmail.com>
2021-01-30 01:05:06 +0530

    offer expended

--- resources/views/store/pages/homepage.blade.php
+++ resources/views/store/pages/homepage.blade.php
@@ -88,6 +88,70 @@
 									</div>
 								</div>
 							</div>
+							<div class="item">
+								<div class="offer-item">								
+									<div class="offer-item-img">	
+										<div class="gambo-overlay"></div>
+										<img src="images/banners/offer-4.jpg" alt="">
+									</div>
+									<div class="offer-text-dt">
+										<div class="offer-top-text-banner">
+											<p>2% Off</p>
+											<div class="top-text-1">Buy More & Save More</div>
+											<span>Beverages</span>
+										</div>
+										<a href="#" class="Offer-shop-btn hover-btn">Shop Now</a>
+									</div>
+								</div>
+							</div>
+							<div class="item">
+								<div class="offer-item">								
+									<div class="offer-item-img">
+										<div class="gambo-overlay"></div>
+										<img src="images/banners/offer-5.jpg" alt="">
+									</div>
+									<div class="offer-text-dt">
+										<div class="offer-top-text-banner">
+											<p>3% Off</p>
+											<div class="top-text-1">Buy More & Save More</div>
+											<span>Nuts & Snacks</span>
+										</div>
+										<a href="#" class="Offer-shop-btn hover-btn">Shop Now</a>
+									</div>
+								</div>
+							</div>
+							<div class="item">
+								<div class="offer-item">								
+									<div class="offer-item-img">	
+										<div class="gambo-overlay"></div>
+										<img src="images/banners/offer-4.jpg" alt="">
+									</div>
+									<div class="offer-text-dt">
+										<div class="offer-top-text-banner">
+											<p>2% Off</p>
+											<div class="top-text-1">Buy More & Save More</div>
+											<span>Beverages</span>
+										</div>
+										<a href="#" class="Offer-shop-btn hover-btn">Shop Now</a>
+									</div>
+								</div>
+							</div>
+							<div class="item">
+								<div class="offer-item">								
+									<div class="offer-item-img">
+										<div class="gambo-overlay"></div>
+										<img src="images/banners/offer-5.jpg" alt="">
+									</div>
+									<div class="offer-text-dt">
+										<div class="offer-top-text-banner">
+											<p>3% Off</p>
+											<div class="top-text-1">Buy More & Save More</div>
+											<span>Nuts & Snacks</span>
+										</div>
+										<a href="#" class="Offer-shop-btn hover-btn">Shop Now</a>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
